2fab910d915f8b6fb3618b1cd5b89ef36db51c22
solzimer <solzimer@gmail.com>
2017-06-02 15:32:20 +0200

    stdev

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+const NOps = require("./nops.js");
+
 const IVAL = 1000;
 const LIST = [];
-
-const DEF_NOPS = ["sum","count","avg"];
+const DEF_NOPS = ["count","sum","avg","stdev"];
 const DEF_COPS = ["sum","count","freq"];
 
 setInterval(()=>{
@@ -9,62 +10,36 @@ setInterval(()=>{
 	LIST.forEach(sws=>{
 		var arr = sws._arr, time = sws._time;
 		var old = [];
+		var oldstats = clone(sws.stats);
 
 		while(arr.length && now-arr[0].t>time)
 			old.push(arr.shift());
 
 		sws._ops.forEach(op=>{
-			sws.stats[op] = NOps[op](sws.stats[op],[],old,sws._arr);
+			sws.stats[op] = NOps[op](sws.stats[op],[],old,sws._arr,sws.stats,oldstats);
 		});
 	});
 },IVAL);
 
-const NOps = {
-	count(currval,newitems,olditems,allitems) {
-		var t = 0, len = allitems.length;
-		for(var i=0;i<len;i++) t += allitems[i].l;
-		return t;
-	},
-	sum(currval,newitems,olditems,allitems) {
-		if(this.d) debugger;
-		if(!olditems.length) {
-			this.d = true;
-		}
-
-		currval = currval===undefined? 0 : currval;
-		var ln = newitems.length, lo = olditems.length;
-		for(let i=0;i<ln;i++) currval += newitems[i].v;
-		for(let i=0;i<lo;i++) currval -= olditems[i].v;
-		//console.log(newitems,olditems,currval);
-		return currval;
-	},
-	avg(currval,newitems,olditems,allitems) {
-		var
-			ln = this.count(0,0,0,newitems), lo = this.count(0,0,0,olditems),
-			nvl = this.count(0,0,0,allitems), ovl = nvl-ln+lo;
-
-		currval = currval===undefined? 0 : currval;
-		currval = currval * ovl;
-
-		for(let i=0;i<newitems.length;i++) currval += newitems[i].v;
-		for(let i=0;i<olditems.length;i++) currval -= olditems[i].v;
-		currval = currval / nvl;
-		return isNaN(currval)? 0 : currval;
-	},
+function clone(obj) {
+	var o = {};
+	for(var i in obj) o[i] = obj[i];
+	return o;
 }
-
 class TimeStats {
 	constructor(time,ops) {
 		this._arr = [];
 		this._time = time || 10000;
 		this._ops = ops || DEF_NOPS;
-		this._step = 1000;
+		this._step = 1;
+		this._oldstats = {};
 		this.stats = {};
 		LIST.push(this);
 	}
 	push(vals) {
 		var now = Date.now();
 		var arr = this._arr;
+		var oldstats = clone(this.stats);
 
 		if(!arr.length) arr.push({t:now,v:0,l:0});
 		var last = arr[arr.length-1];
@@ -77,13 +52,13 @@ class TimeStats {
 			var oa = [arr.pop()], na = [last];
 			arr.push(last);
 			this._ops.forEach(op=>{
-				this.stats[op] = NOps[op](this.stats[op],na,oa,arr);
+				this.stats[op] = NOps[op](this.stats[op],na,oa,arr,this.stats,oldstats);
 			});
 		}
 		else {
 			vals.forEach(v=>{arr.push(v)});
 			this._ops.forEach(op=>{
-				this.stats[op] = NOps[op](this.stats[op],vals,[],arr);
+				this.stats[op] = NOps[op](this.stats[op],vals,[],arr,this.stats,oldstats);
 			});
 		}
 
@@ -119,9 +94,19 @@ class SizeStats {
 var a = new TimeStats;
 
 setInterval(()=>{
-	a.push(Math.random());
+	a.push(Math.random()*10);
 });
 
 setInterval(()=>{
 	console.log(a.stats);
 },100);
+
+/*
+var a = [1,3,4,7,8,1,3,2,2,2,4,3];
+var avg = a.reduce((curr,val)=>curr+val,0) / a.length;
+var asum = a.reduce((curr,val)=>curr+Math.pow(val-avg,2),0);
+var bsum = a.reduce((curr,val)=>curr+val*val,0) + avg*avg*a.length - 2*a.reduce((curr,val)=>curr+val*avg,0);
+var stdev1 = Math.sqrt(asum/a.length);
+var stdev2 = Math.sqrt(bsum/a.length);
+console.log(avg,stdev1,stdev2);
+*/
---
+++ nops.js
@@ -0,0 +1,64 @@
+module.exports = {
+	count(currval,newitems,olditems,allitems,newstats,oldstats) {
+		var t = 0, len = allitems.length;
+		for(var i=0;i<len;i++) t += (allitems[i].l||1);
+		return t;
+	},
+	sum(currval,newitems,olditems,allitems,newstats,oldstats) {
+		currval = currval===undefined? 0 : currval;
+		var ln = newitems.length, lo = olditems.length;
+		for(let i=0;i<ln;i++) currval += newitems[i].v;
+		for(let i=0;i<lo;i++) currval -= olditems[i].v;
+		//console.log(newitems,olditems,currval);
+		return currval;
+	},
+	avg(currval,newitems,olditems,allitems,newstats,oldstats) {
+		var
+			ln = this.count(0,0,0,newitems), lo = this.count(0,0,0,olditems),
+			nvl = this.count(0,0,0,allitems), ovl = nvl-ln+lo;
+
+		currval = currval===undefined? 0 : currval;
+		currval = currval * ovl;
+
+		for(let i=0;i<newitems.length;i++) currval += newitems[i].v;
+		for(let i=0;i<olditems.length;i++) currval -= olditems[i].v;
+		currval = currval / nvl;
+		return isNaN(currval)? 0 : currval;
+	},
+	stdev(currval,newitems,olditems,allitems,newstats,oldstats) {
+		oldstats.stdev = oldstats.stdev || {avg:1,sqsum:0,sum:0,stdev:0};
+
+		var
+			ln = this.count(0,0,0,newitems), lo = this.count(0,0,0,olditems),
+			nvl = this.count(0,0,0,allitems), ovl = nvl-ln+lo,
+			len = allitems.length,
+			oavg = oldstats.avg || 0;
+
+		var oldavg = oldstats.stdev.avg;
+		var oldsqsum = oldstats.stdev.sqsum;
+		var oldsum = oldstats.stdev.sum;
+		var newavg = newstats.avg || 0;
+		var newsqsum = oldsqsum;
+		var newsum = oldsum;
+
+		for(let i=0;i<olditems.length;i++) {
+			let it = olditems[i].v / (olditems[i].l||1);
+			newsqsum -= it*it;
+			newsum -= it;
+		}
+
+		for(let i=0;i<newitems.length;i++) {
+			let it = newitems[i].v / (newitems[i].l||1);
+			newsqsum += it*it;
+			newsum += it;
+		}
+
+		var stdev = Math.sqrt((newsqsum - 2*newavg*newsum + len*newavg*newavg) / len);
+		return {
+			avg : newavg,
+			sqsum : newsqsum,
+			sum : newsum,
+			stdev : stdev
+		}
+	}
+}
